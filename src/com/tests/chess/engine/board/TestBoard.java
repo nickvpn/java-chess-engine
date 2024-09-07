6b141d1acee7c0131e25e9cbcf27b474a7ea8724
nickvpn <nwschool66@gmail.com>
2024-09-07 00:30:53 -0400

    fools mate test case

--- src/com/tests/chess/engine/board/TestBoard.java
+++ src/com/tests/chess/engine/board/TestBoard.java
@@ -1,6 +1,11 @@
 package com.tests.chess.engine.board;
 
 import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+import com.chess.engine.player.MoveTransition;
+import com.chess.engine.player.ai.MiniMax;
+import com.chess.engine.player.ai.MoveStrategy;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,6 +32,43 @@ public class TestBoard {
 //        assertTrue(board.currentPlayer().getOpponent().isQueenSideCastleCapable());
 //          assertEquals(StandardBoardEvaluator.get().evaluate(board, 0), 0);
 
+    }
+
+    // fools mate test >:)
+    @Test
+    public void testFoolsMate(){
+
+        final Board board = Board.createStandardBoard();
+        final MoveTransition t1 = board
+                .currentPlayer()
+                .makeMove(Move.MoveFactory
+                .createMove(board,
+                    BoardUtils.getCoordinateAtPosition("f2"),
+                    BoardUtils.getCoordinateAtPosition("f3")));
+        assertTrue(t1.getMoveStatus().isDone());
+
+        final MoveTransition t2 = t1.getTransitionBoard()
+                .currentPlayer()
+                .makeMove(Move.MoveFactory
+                        .createMove(t1.getTransitionBoard(),
+                        BoardUtils.getCoordinateAtPosition("e7"),
+                        BoardUtils.getCoordinateAtPosition("e5")));
+        assertTrue(t2.getMoveStatus().isDone());
+
+        final MoveTransition t3 = t2.getTransitionBoard()
+                .currentPlayer()
+                .makeMove(Move.MoveFactory
+                        .createMove(t2.getTransitionBoard(),
+                                BoardUtils.getCoordinateAtPosition("g2"),
+                                BoardUtils.getCoordinateAtPosition("g4")));
+        assertTrue(t3.getMoveStatus().isDone());
+
+        final MoveStrategy strategy = new MiniMax(4);
+        final Move aiMove = strategy.execute(t3.getTransitionBoard());
+        final Move bestMove = Move.MoveFactory.createMove(t3.getTransitionBoard(), BoardUtils.getCoordinateAtPosition("d8"),
+                BoardUtils.getCoordinateAtPosition("h4"));
+
+        assertEquals(aiMove, bestMove);
     }
 
 }
